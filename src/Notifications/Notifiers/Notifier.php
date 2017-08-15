0803d38cc616bbde63baa1f4b1de629b465e192c
terence1990 <terence@creativelittledots.co.uk>
2017-08-15 15:28:20 +0100

    Fixed render method

--- src/Notifications/Notifiers/Notifier.php
+++ src/Notifications/Notifiers/Notifier.php
@@ -149,7 +149,8 @@
 	     * @return void
 	     */
 	    public function render( $item = array() ) {
-		    $items = $item ? ( is_array( $item ) ? $item : [$item] ) : $this->all();
+		    $keys = array_keys( $item );
+		    $items = $item ? ( is_numeric( reset( $keys ) ) ? $item : [$item] ) : $this->all();
 		    $output = implode( array_map( [ $this, 'build' ], $items ) );
 		    $this->clear();
 		    return $output;
